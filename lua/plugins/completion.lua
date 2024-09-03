aaca06368c4b0425aa4e1223e2ab5b5d327adcfa
AymericHenouille <aymeric.henouille@gmail.com>
2024-09-03 10:56:17 +0200

    feat: comment lua plugin

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -33,11 +33,36 @@ return {
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
-        },
-        {
-          { name = 'buffer' },
-        })
+        }, {{ name = 'buffer' }})
       })
     end
   },
 }
+
+-- local kind_icons = {
+--   Text = "",
+--   Method = "󰆧",
+--   Function = "󰊕",
+--   Constructor = "",
+--   Field = "󰇽",
+--   Variable = "󰂡",
+--   Class = "󰠱",
+--   Interface = "",
+--   Module = "",
+--   Property = "󰜢",
+--   Unit = "",
+--   Value = "󰎠",
+--   Enum = "",
+--   Keyword = "󰌋",
+--   Snippet = "",
+--   Color = "󰏘",
+--   File = "󰈙",
+--   Reference = "",
+--   Folder = "󰉋",
+--   EnumMember = "",
+--   Constant = "󰏿",
+--   Struct = "",
+--   Event = "",
+--   Operator = "󰆕",
+--   TypeParameter = "󰅲",
+-- }
