881cba40b5ea3c139fb7803ec5867a09b2492273
AymericHenouille <aymeric.henouille@gmail.com>
2025-03-04 16:51:08 +0100

    feat: first version with ewok

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -1,3 +1,5 @@
+print("ewok")
+
 return {
   "lewis6991/gitsigns.nvim",
   dependencies = {
@@ -5,24 +7,25 @@ return {
   },
   event = { "BufEnter" },
   opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
     auto_attach = true,
+    signcolumn = true,
+    numhl = false,
+    linehl = false,
+    word_diff = false,
+    current_line_blame = true,
     on_attach = function(buf)
       local gitsigns = require("gitsigns")
       local nmap = require("features.map").nmap
-
       nmap("]c", function()
-        gitsigns.nav_hunk("next")
+        if not vim.wo.diff then
+          gitsigns.nav_hunk("next")
+        end
       end, "Navigate to the next git hunk")
 
       nmap("[c", function()
-        gitsigns.nav_hunk("previous")
+        if not vim.wo.diff then
+          gitsigns.nav_hunk("prev")
+        end
       end, "Navigate to the previous git hunk")
     end
   },
