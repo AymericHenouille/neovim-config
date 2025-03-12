bff897eb084e04511f7ea788ada7758cc7f1ca55
AymericHenouille <aymeric.henouille@gmail.com>
2025-03-12 16:55:34 +0100

    feat: scope

--- lua/plugins/buffers.lua
+++ lua/plugins/buffers.lua
@@ -1,23 +1,30 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = function()
-    local highlights = require("catppuccin.groups.integrations.bufferline")
-    local palettes = require("catppuccin.palettes")
-    local frappe = palettes.get_palette("frappe")
-    return {
-      highlights = highlights.get({
-        custom = {
-          frappe = {
-            fill = { bg = frappe.surface1 }
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    opts = function()
+      local highlights = require("catppuccin.groups.integrations.bufferline")
+      local palettes = require("catppuccin.palettes")
+      local frappe = palettes.get_palette("frappe")
+      return {
+        diagnostics = "nvim_lsp",
+        highlights = highlights.get({
+          custom = {
+            frappe = {
+              fill = { bg = frappe.surface1 },
+              indicator_selected = { sp = frappe.flamingo },
+              buffer_selected = { sp = frappe.flamingo },
+              TabLineSel = { bg = frappe.flamingo },
+            },
           },
+        }),
+        options = {
+          separator_style = "slant",
         },
-      }),
-      options = {
-        separator_style = "slant",
-      },
-    }
-  end,
+      }
+    end,
+  },
 }
 
