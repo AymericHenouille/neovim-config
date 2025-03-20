3d1f263e3609633176fa0a29aedebfb2a3eeadca
AymericHenouille <aymeric.henouille@gmail.com>
2025-03-20 11:21:08 +0100

    feat: diagnostic support

--- lua/mappings/lsp.lua
+++ lua/mappings/lsp.lua
@@ -1,3 +1,4 @@
+local hasfloatingwin = require("features.win").hasfloatingwin
 local map = require("features.map")
 local nmap = map.nmap
 
@@ -9,7 +10,16 @@ nmap("<LEADER>gt", "<cmd>Telescope lsp_type_definitions<CR>", "Show LSP type def
 nmap("<LEADER>ca", vim.lsp.buf.code_action, "See available code actions")
 nmap("<LEADER>rn", vim.lsp.buf.rename, "Smart rename")
 nmap("<LEADER>D", "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics")
+nmap("<LEADER>d", function()
+  if vim.diagnostic.is_enabled() then
+    if not hasfloatingwin() then
+      vim.diagnostic.open_float()
+    end
+  end
+end, "Show buffer diagnostics in float")
+
 nmap("[d", vim.diagnostic.goto_prev, "Go to previous diagnostic")
 nmap("]d", vim.diagnostic.goto_next, "Go to next diagnostic")
+
 nmap("K", vim.lsp.buf.hover, "Show documentation for what is under cursor")
 nmap("<LEADER>rs", ":LspRestart<CR>", "Restart LSP")
