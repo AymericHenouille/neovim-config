4e8f862628b2df083578dd833f047bc14810deef
AymericHenouille <aymeric.henouille@gmail.com>
2025-05-09 15:24:04 +0200

    feat: theme switch gnome support

--- lua/plugins/themes/init.lua
+++ lua/plugins/themes/init.lua
@@ -1,6 +1,6 @@
 return {
-  require("plugins.themes.lualine"),
   require("plugins.themes.everforest"),
+  require("plugins.themes.lualine"),
   require("plugins.themes.alpha"),
   require("plugins.themes.theme_switch"),
 }
--- lua/plugins/themes/theme_switch.lua
+++ lua/plugins/themes/theme_switch.lua
@@ -1,13 +1,24 @@
 return {
  "f-person/auto-dark-mode.nvim",
-  opts = {
-    set_dark_mode = function()
-      vim.o.background = "dark"
-    end,
-    set_light_mode = function()
-      vim.o.background = "light"
-    end,
-    update_interval = 3000,
-    fallback = "light"
-  },
+  opts = function()
+    local fallback = "light"
+    local handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme")
+    if handle ~= nil then
+      local prefer = handle:read("*a")
+      if string.find(prefer, "dark") then
+        fallback = "dark"
+      end
+      handle:close()
+    end
+    return {
+      set_dark_mode = function()
+        vim.o.background = "dark"
+      end,
+      set_light_mode = function()
+        vim.o.background = "light"
+      end,
+      fallback = fallback,
+      update_interval = 3000,
+    }
+  end,
 }
